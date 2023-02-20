b8a7df249b10b7e5e7e1473c0f94b0fdfd2c2b49
k5-salman <salmankhalid518@gmail.com>
2023-02-20 15:39:48 +0500

    End of section 12

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -55,6 +55,11 @@ user: User | undefined;
       if (response) {
         const photo = JSON.parse(response);
         this.member?.photos.push(photo);
+        if (photo.isMain && this.user && this.member) {
+          this.user.photoUrl = photo.url;
+          this.member.photoUrl = photo.url;
+          this.accountService.setCurrentUser(this.user)
+        }
       }
     }
   }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -18,10 +18,7 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        this.router.navigateByUrl('/members');
-        console.log(response);
-      }
+      next: response => this.router.navigateByUrl('/members')
     });
   }
 
